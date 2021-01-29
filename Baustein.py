47cbb26427e19277245639c80d596942ace53deb
Peter Schwarz <p.schwarz1994@outlook.de>
2021-01-29 20:57:33 +0100

    class file & Test class file createt

---
+++ Baustein.py
@@ -0,0 +1,23 @@
+class ParaData:
+
+    def __init__(self) -> None:
+        self.read_out = {}
+
+    @property
+    def Sections(self):
+        return self._get_Sections()
+
+    @Sections.setter
+    def Sections(self, new_para_data):
+        return self._set_Sections(new_para_data)
+
+    def _set_Sections(self, new_para_data):
+        self.read_out = {}
+        para_data = new_para_data[2:].copy()
+        for count, element in enumerate(para_data, start=0):
+            if count % 5 == 0:
+                self.read_out.update({element: para_data[count:count + 5]})
+        return self.read_out
+
+    def _get_Sections(self):
+        return self.read_out
